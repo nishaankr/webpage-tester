3612a1abab7b32bf17713fb2e9c7d8754f62df7a
nishaankr <nishaankr@gmail.com>
2021-04-07 10:47:01 +0400

    Added inner divs

--- js/inject.js
+++ js/inject.js
@@ -28,11 +28,34 @@
 // console.log(banFunctions);
 // space[0].appendChild(banFunctions.createDivElement());
 
-var banFunc = document.createElement("div");
-banFunc.innerHTML = " ";
-banFunc.className = "banFunction";
-
+//main element where the entire form goes into.
 let space = document.getElementsByClassName("emptySpace");
 console.log(space.length);
 
+//main div with all details
+var banFunc = document.createElement("div");
+banFunc.innerHTML = " ";
+banFunc.className = "banFunction divFunction";
+
+//div with name input field
+var userNameDiv = document.createElement("div");
+userNameDiv.innerHTML =
+  "<input class='playerName inputF' type='text' placeholder='Username'>";
+userNameDiv.className = "name divFunction";
+
+//div with reason input field and timers
+var reasonDiv = document.createElement("div");
+reasonDiv.innerHTML =
+  "<input class='reason inputF' type='text' placeholder='Reason'>";
+reasonDiv.className = "reason divFunction";
+
+var evidenceDiv = document.createElement("div");
+evidenceDiv.innerHTML =
+  "<input class='evidence inputF' type='text' placeholder='Evidence'>";
+evidenceDiv.className = "evidence divFunction";
+
+//appending elemenets created to the mainpage
 space[0].appendChild(banFunc);
+banFunc.appendChild(userNameDiv);
+banFunc.appendChild(reasonDiv);
+banFunc.appendChild(evidenceDiv);
